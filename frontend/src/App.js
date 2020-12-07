af62f855e29a4ff290063d0a20a10dd9340b5638
Rachel <rjcrter11@gmail.com>
2020-12-07 11:35:12 -0600

    adds react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -6,16 +7,16 @@ import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3' >
         <Container>
-          <HomeScreen />
+          <Route exact path='/' component={HomeScreen} />
         </Container>
 
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
