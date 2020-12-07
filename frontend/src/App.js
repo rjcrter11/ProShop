7a45a2856235a4d700ac52a4eaf38221d4fce03d
Rachel <rjcrter11@gmail.com>
2020-12-07 09:24:26 -0600

    installs react-bootstrap and bootswatch theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
       <Header />
       <main>
-        <h1>Welcome to ProShop</h1>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+
       </main>
       <Footer />
     </>
