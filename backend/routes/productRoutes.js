941d915b388bd1cf2604da544b7083fe8077626a
Rachel <rjcrter11@gmail.com>
2020-12-16 12:04:04 -0600

    moves functionality to contollers, cleans up routes file for products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,32 +1,11 @@
 import express from 'express';
-import asyncHndler from 'express-async-handler';
-import Product from '../models/productModel.js';
+import { getProductById, getProducts } from '../controllers/productController.js';
 
 const router = express.Router();
 
-// @desc  Fetch all Products 
-// @route  GET /api/products
-// @access  Public
-router.get('/', asyncHndler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-}));
 
-// @desc  Fetch all Products 
-// @route  GET /api/products/:id
-// @access  Public
-router.get(
-    '/:id',
-    asyncHndler(async (req, res) => {
-        const product = await Product.findById(req.params.id);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
-        if (product) {
-            res.json(product)
-        } else {
-            res.status(404);
-            throw new Error('Product not found');
 
-        }
-    })
-)
-export default router; 
+export default router
