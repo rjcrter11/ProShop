a2b0c937041607f78e2e0aeb6551b61d4782a277
Rachel <rjcrter11@gmail.com>
2020-12-29 08:54:56 -0600

    updates user profile and sets new user info to local storage

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -11,7 +11,8 @@ import {
     USER_DETAILS_FAIL,
     USER_UPDATE_PROFILE_REQUEST,
     USER_UPDATE_PROFILE_SUCCESS,
-    USER_UPDATE_PROFILE_FAIL
+    USER_UPDATE_PROFILE_FAIL,
+    USER_UPDATE_PROFILE_RESET
 } from '../constants/userConstants';
 
 export const userLoginReducer = (state = { products: [] }, action) => {
@@ -52,7 +53,7 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
             return state
     }
 };
-export const userUdateProfileReducer = (state = {}, action) => {
+export const userUpdateProfileReducer = (state = {}, action) => {
     switch (action.type) {
         case USER_UPDATE_PROFILE_REQUEST:
             return { loading: true }
@@ -60,6 +61,8 @@ export const userUdateProfileReducer = (state = {}, action) => {
             return { loading: false, success: true, userInfo: action.payload }
         case USER_UPDATE_PROFILE_FAIL:
             return { loading: false, error: action.payload }
+        case USER_UPDATE_PROFILE_RESET:
+            return {}
         default:
             return state
     }
